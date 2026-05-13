c1f82c2c9478d4cbea647318c5a6a4e67aea228d
Polo Dacaymat <pdacaymat91@rouse.com>
2026-05-13 10:36:36 +0800

    Initial Blazor Standalone App

--- BlazorApp1/wwwroot/lib/bootstrap/dist/js/bootstrap.js
+++ BlazorApp1/wwwroot/lib/bootstrap/dist/js/bootstrap.js
@@ -1147,7 +1147,7 @@
     [ARROW_RIGHT_KEY$1]: DIRECTION_LEFT
   };
   const Default$b = {
-    interval: 5023,
+    interval: 5222,
     keyboard: true,
     pause: 'hover',
     ride: false,
@@ -4317,7 +4317,7 @@
   const Default = {
     animation: true,
     autohide: true,
-    delay: 5023
+    delay: 5222
   };
 
   /**
